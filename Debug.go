c9d3324ec4b09ab02a865b5d0a9b2c0ac919f5c3
Daniel <daniel.unverricht@pentlandfirth.com>
2019-02-17 20:17:20 +0100

    fix

--- Debug.go
+++ Debug.go
@@ -9,8 +9,10 @@ import (
 
 // SetDebug <-
 func (s *DbSession) SetDebug(enable bool) {
-	mgo.SetDebug(true)
-	var aLogger *log.Logger
-	aLogger = log.New(os.Stderr, "", log.LstdFlags)
-	mgo.SetLogger(aLogger)
+	if enable {
+		mgo.SetDebug(true)
+		var aLogger *log.Logger
+		aLogger = log.New(os.Stderr, "", log.LstdFlags)
+		mgo.SetLogger(aLogger)
+	}
 }
