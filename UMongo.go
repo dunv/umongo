4d33fd982f79e894947a1f600fb2ac440a2577e0
Daniel <daniel.unverricht@pentlandfirth.com>
2019-11-16 10:28:37 +0100

    add debugging

--- UMongo.go
+++ UMongo.go
@@ -1,7 +1,21 @@
 package umongo
 
-import "github.com/dunv/ulog"
+import (
+	"fmt"
+
+	"github.com/dunv/ulog"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 func init() {
 	ulog.AddReplaceFunction("github.com/dunv/umongo.ModelService.EnsureIndexes", "umongo.EnsureIndexes")
+}
+
+func DebugQuery(query interface{}) {
+	rendered, err := bson.MarshalExtJSON(query, false, false)
+	if err != nil {
+		fmt.Println("DEBUG: err marshaling", err)
+	} else {
+		fmt.Println("DEBUG: rendered", string(rendered))
+	}
 }
