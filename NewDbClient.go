47c839c7472be7511392db9def9dfce7f7949d54
Daniel <daniel.unverricht@pentlandfirth.com>
2020-03-30 17:44:23 +0200

    add different default type

--- NewDbClient.go
+++ NewDbClient.go
@@ -2,16 +2,30 @@ package umongo
 
 import (
 	"context"
+	"reflect"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/bsontype"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func NewDbClient(connectionString string, timeout time.Duration) (*mongo.Client, context.CancelFunc, error) {
+	// Register mapType as default EmbeddedDocument "marshalInto"-type
+	// That way if we specify an interface to be rendered into we do not end up with
+	// { "Key": "xxx", "Value": "yyy"} but with { "xxx": "yyy" }
+	// https://jira.mongodb.org/browse/GODRIVER-988
+	tM := reflect.TypeOf(bson.M{})
+	reg := bson.NewRegistryBuilder().RegisterTypeMapEntry(bsontype.EmbeddedDocument, tM).Build()
+
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connectionString))
+	client, err := mongo.Connect(
+		ctx,
+		options.Client().ApplyURI(connectionString),
+		options.Client().SetRegistry(reg),
+	)
 	if err != nil {
 		return nil, cancel, err
 	}
